037180fffd2e4cbb61ddf373bae0009e26fc13ef
Nabila <nowshinnabila24@gmail.com>
2024-09-23 22:30:33 +0600

    donation done partially

--- js/features.js
+++ js/features.js
@@ -1,3 +1,5 @@
+// connection
+
 function blog(){
     window.location.href = "./blog.html";
 }
@@ -6,6 +8,8 @@ function home(){
     window.location.href = "./index.html";
 }
 
+
+// blur
 
 window.addEventListener('scroll', function() {
     const blurElement = document.getElementById('blur');
@@ -19,3 +23,44 @@ window.addEventListener('scroll', function() {
 });
 
 
+// donation
+
+document.getElementById('btnDonation').addEventListener('click', function(){
+      document.getElementById('btnDonation').classList.add('bg-[#B4F461]');
+      document.getElementById('btnDonation').classList.remove('bg-white');
+      document.getElementById('btnDonation').classList.remove('border-slate-300');
+      document.getElementById('btnHistory').classList.add('bg-white');
+      document.getElementById('btnHistory').classList.add('border-slate-300');
+})
+
+document.getElementById('btnHistory').addEventListener('click', function(){
+      document.getElementById('btnHistory').classList.add('bg-[#B4F461]');
+      document.getElementById('btnHistory').classList.remove('bg-white');
+      document.getElementById('btnHistory').classList.remove('border-slate-300');
+      document.getElementById('btnDonation').classList.add('border-slate-300');
+      document.getElementById('btnDonation').classList.remove('bg-[#B4F461]');
+      document.getElementById('btnDonation').classList.add('bg-white');
+})
+
+
+
+
+document.getElementById('noakhali').addEventListener('click', function(){
+
+    const amount = donation('noakhaliDonation');
+    const addNDonation = parseFloat(document.getElementById('addNDonation').innerText);
+    const sum = amount + addNDonation;
+    const newBalance = sum.toFixed(2);
+    const myBalance = parseFloat(document.getElementById('myBalance').innerText);
+    const myNewBalance = myBalance - amount;
+   
+    if(isNaN(newBalance) || amount<=0){
+        alert('Failed to donate.');
+    }
+    else{
+        document.getElementById('addNDonation').innerText = newBalance;
+    }
+    
+    document.getElementById('noakhaliDonation').value= '';
+    document.getElementById('myBalance').innerText = myNewBalance;
+})
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,4 @@
-
+function donation(id){
+    const amount = parseFloat(document.getElementById(id).value);
+    return parseFloat(amount.toFixed(2));
+}
