e92c0bea42768e0fe573ea1745f20b4955773270
Nabila <nowshinnabila24@gmail.com>
2024-09-23 15:30:40 +0600

    blur effect done

--- js/features.js
+++ js/features.js
@@ -5,3 +5,17 @@ function blog(){
 function home(){
     window.location.href = "../index.html";
 }
+
+
+window.addEventListener('scroll', function() {
+    const blurElement = document.getElementById('blur');
+   
+    if (window.scrollY > 0) {
+        blurElement.classList.remove('bg-[#F9F7F3]');
+    } 
+    else {
+        blurElement.classList.add('bg-[#F9F7F3]');
+    }
+});
+
+
---
+++ js/utilities.js
@@ -0,0 +1,5 @@
+// function scroll(id){
+//     const getBlur = document.getElementById(id);
+//     const blur = getBlur.classList.add('bg-none');
+//     return blur;
+// }
