bc5a92ac5723896883c0c503692497f6c92269af
Nabila <nowshinnabila24@gmail.com>
2024-09-23 16:35:31 +0600

    updated

--- js/features.js
+++ js/features.js
@@ -1,9 +1,9 @@
 function blog(){
-    window.location.href = "../blog.html";
+    window.location.href = "./blog.html";
 }
 
 function home(){
-    window.location.href = "../index.html";
+    window.location.href = "./index.html";
 }
 
 
